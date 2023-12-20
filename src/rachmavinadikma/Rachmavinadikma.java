4fa5ea3a91d60cfb3dfecbecc1dcc9a8d563a4f2
vinadikma <rachmavina19@gmail.com>
2023-12-20 12:32:40 +0700

    TugasPBO11

---
+++ src/rachmavinadikma/Rachmavinadikma.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package rachmavinadikma;
+
+/**
+ *
+ * @author ACER
+ */
+public class Rachmavinadikma {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Login lg = new Login();
+            lg.setVisible(true);
+            lg.pack();
+            lg.setLocation(null);
+            lg.setDefaultCloseOperation(Register.EXIT_ON_CLOSE);
+            
+    }
+    
+}
